92cc70b02879ca3e93e331aaed79db2735c8ecc5
ZNBai <bzn991206@gmail.com>
2023-03-08 16:54:25 +0100

    Modified scholar functions

--- rcn_py/scholar.py
+++ rcn_py/scholar.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import itertools
+from pyvis.network import Network
 from scholarly import scholarly
 
 
@@ -18,7 +19,6 @@ def scholar_search_coauthor(author_fullname):
     # Filter by year
     new_docs = []
     for j in docs:
-        if ('pub_year' in j.keys()) and (j['pub_year'] > '2018'):
             new_docs.append(j)       
         
     # Search the authors of each publication
@@ -44,3 +44,59 @@ def scholar_search_coauthor(author_fullname):
         
     return df, link
 
+
+def scholar_build_network(fullname, node, link, au_group):
+    sources = []
+    targets = []
+    weights = []
+
+    for i in link:
+        sources.append(i[0])
+        targets.append(i[1])
+        weights.append(link.count(i))
+    
+    # Pyvis network
+    N = Network(height=800, width="100%", bgcolor="#222222", font_color="white")
+    N.toggle_hide_edges_on_drag(False)
+    N.barnes_hut()
+
+    edge_data = zip(sources, targets, weights)
+
+    for e in edge_data:
+        src = e[0]
+        dst = e[1]
+        w = e[2]
+
+        N.add_node(src, src, title=src, group = au_group[src])
+        N.add_node(dst, dst, title=dst, group = au_group[dst])
+        N.add_edge(src, dst, value=w)
+
+    neighbor_map = N.get_adj_list()
+
+    # add neighbor data to node hover data
+    for node in N.nodes:
+        neighbors = []
+        #node["title"] = " Neighbors: \n" + " \n".join(neighbors)
+        node["title"] = "Link to the author’s page:\n" + scholarly.search_author_id(node['id'])['url_picture']
+        node["value"] = scholarly.search_author_id(node['id'])['citedby']
+        node["label"] = scholarly.search_author_id(node['id'])['name']
+       
+
+    N.show(fullname + ".html")
+
+
+def build_graph(G, sch_id, depth: int, bredth: int, last_author=[]):
+    author_search = scholarly.search_author_id(sch_id)
+    author = scholarly.fill(author_search,sections=['coauthors','indices','counts'])
+    author_name = author['name']
+    author_hin = author['hindex']
+    author_ncite = author['citedby']
+    G.add_node(author_name,hindex=author_hin,name=author_name,ncite=author_ncite)
+    print("now at %s, depth=%d"%(author_name,depth))
+    if depth>0:
+        for coauthor in author['coauthors'][:int(bredth)]:
+            G.add_edge(author_name,coauthor['name'])
+            if coauthor['name'] not in last_author:
+                last_author.append(author_name)
+                build_graph(G,coauthor['scholar_id'],depth-1,bredth,last_author)
+    
