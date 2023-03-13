6f7eef2b47dcd1a4ac34e9234431c5ce9beb2c70
ZNBai <bzn991206@gmail.com>
2023-03-13 10:03:34 +0100

    basic data storage packages

--- rcn_py/orcid.py
+++ rcn_py/orcid.py
@@ -1,7 +1,19 @@
 from crossref.restful import Works
 import requests
 import pandas as pd
-import itertools
+import nltk
+import re
+from nltk.corpus import stopwords
+nltk.download("stopwords")
+stop_words = set(stopwords.words("english"))
+from nltk.stem import PorterStemmer
+from nltk.tokenize import word_tokenize
+ps = PorterStemmer()
+from itertools import combinations
+import numpy as np
+import gensim
+from gensim import corpora
+from pyvis.network import Network
 
 # Query ORCID by a fullname
 headers = {
@@ -79,27 +91,161 @@ def extract_doi(work):
     doi = str(doi)
     return doi, title
 
-def get_coauthors(full_name):
+def orcid_get_coauthors(full_name):
     orcid_id = name_to_orcid_id(full_name)
     orcid_record = query_orcid_for_record(orcid_id)
     docs = extract_works_section(orcid_record)
+    
     all_orcid = []
     all_names = []
+    all_group = []
     coauthor_links = []
+
+    # LDA topic modeling
+    doi_list = []
+    for doc in docs:
+        doi, title = extract_doi(doc)
+        doi_list.append(doi)
+    cluster_dict = orcid_lda_cluster(doi_list)
         
     for doc in docs:
         doi, title = extract_doi(doc)
+        doc_group = cluster_dict[doi]
         orcid_list, name_list = get_authors(doi)
+        # Geo-filtering 
+        filtered_orcid_list = []
+        filtered_name_list = []
+        author_group_list = []
+        for i in range(len(orcid_list)):
+            # orcid_record = orcid.query_orcid_for_record(orcid_list[i])
+            # if orcid_record['person']['addresses']['address']:
+            #     country = orcid_record['person']['addresses']['address'][0]['country']['value']
+            # else:
+            #     country = ''
+            # if country == 'NL':
+                filtered_orcid_list.append(orcid_list[i])
+                filtered_name_list.append(name_list[i])
+                author_group_list.append(doc_group)
 
-        coauthor_links = coauthor_links+list(itertools.combinations(orcid_list, 2))
+        # Combination
+        if len(filtered_orcid_list) >=2:
+            coauthor_links = coauthor_links+list(combinations(filtered_orcid_list, 2))
 
-        all_orcid += orcid_list
-        all_names += name_list
+        all_orcid += filtered_orcid_list
+        all_names += filtered_name_list
+        all_group += author_group_list
+
+    group_temp = []
+    new_group_num = []
+    for g in all_group:
+        if g not in group_temp:
+            group_temp.append(g)
+        new_group_num.append(group_temp.index(g))
         
     df = pd.DataFrame()
     df['orcid'] = all_orcid
     df['name'] = all_names
+    df['group'] = new_group_num
+
     new_df = df.drop_duplicates(subset = ['orcid'],keep='first', ignore_index=True)
     
     return new_df, coauthor_links
+
+def clean_text(text):
+    text = text.replace('\n'," ") 
+    text = re.sub(r"-", " ", text) 
+    text = re.sub(r"\d+/\d+/\d+", "", text) 
+    text = re.sub(r"[0-2]?[0-9]:[0-6][0-9]", "", text) 
+    text = re.sub(r"[\w]+@[\.\w]+", "", text) 
+    text = re.sub(r"/[a-zA-Z]*[:\//\]*[A-Za-z0-9\-_]+\.+[A-Za-z0-9\.\/%&=\?\-_]+/i", "", text)
+    pure_text = ''
+    for letter in text:
+        # Leave only letters and spaces
+        if letter.isalpha() or letter==' ':
+            letter = letter.lower()
+            pure_text += letter
+            
+    corpus_lst = [ps.stem(word) for word in pure_text.split() if word not in stop_words]
+    return corpus_lst
     
+
+def orcid_lda_cluster(dois):
+    
+    cleaned_abs_corpus = []
+    clusters = {}
+    works = Works()
+    for i in dois:
+        w = works.doi(i)
+        if 'abstract' in w.keys():
+            cleaned_abs_corpus.append(clean_text(w['abstract']))
+        elif 'title' in w.keys() and w['title']:
+            cleaned_abs_corpus.append(clean_text(w['title'][0]))
+        else:
+            cleaned_abs_corpus.append([])
+
+    num_topics = 10
+    dictionary = corpora.Dictionary(cleaned_abs_corpus) 
+    corpus = [dictionary.doc2bow(text) for text in cleaned_abs_corpus]
+
+    passes= 150
+    np.random.seed(1)
+
+    lda_model = gensim.models.LdaMulticore(corpus=corpus,
+                                           id2word=dictionary,
+                                           num_topics=num_topics,
+                                           chunksize= 4000, 
+                                           batch= True,
+                                           minimum_probability=0.001,
+                                           iterations=350,
+                                           passes=passes)
+    group_num = []
+    for i in range(len(cleaned_abs_corpus)):
+        scores = []
+        for j1,j2 in lda_model[corpus[i]]:
+            scores.append(j2)
+            clusters[dois[i]] = scores.index(max(scores))
+        group_num.append(scores.index(max(scores)))
+        
+    return clusters
+
+def orcid_network(name, author_df, link):
+    
+    sources = []
+    targets = []
+    weights = []
+
+    for i in link:
+        sources.append(i[0])
+        targets.append(i[1])
+        weights.append(link.count(i))
+
+    id2name = {}
+    id2group = {}
+    for index in range(author_df.shape[0]):
+        id2name[author_df['orcid'][index]] = author_df['name'][index]
+        id2group[author_df['orcid'][index]] = int(author_df['group'][index])
+    
+    # Pyvis network
+    N = Network(height=800, width="100%", bgcolor="#222222", font_color="white")
+    N.toggle_hide_edges_on_drag(False)
+    N.barnes_hut()
+
+    edge_data = zip(sources, targets, weights)
+
+    for e in edge_data:
+        src = e[0]
+        dst = e[1]
+        w = e[2]
+
+        N.add_node(src, src, title=src, group=id2group[src])
+        N.add_node(dst, dst, title=dst, group=id2group[dst])
+        N.add_edge(src, dst, value=w)
+
+
+    # add neighbor data to node hover data
+    for node in N.nodes:
+        node["title"] = node["id"]
+        node["label"] = id2name[node['id']]
+       
+
+    N.show(name+".html")
